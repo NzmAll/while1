3bcace13c567bdec56155828cc39029c7c74519f
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-03-22 13:07:40 +0400

    --

--- Holiday/Holiday/Program.cs
+++ Holiday/Holiday/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
+            int total;
+            Console.Write("Ededi daxil edin: ");
+            int number = int.Parse(Console.ReadLine()!);
 
+            for(int i = 0; i <= 10; i++)
+            {
+                if(i >= 0)
+                {
+                    total = number * i;
+                    Console.WriteLine("Hasil: " + total);
+                }
+            }
 
         }
     }
