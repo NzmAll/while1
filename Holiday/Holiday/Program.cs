0d73738560ef3eb88d282a322b513418b8556672
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-03-22 15:42:57 +0400

    holidays

--- Holiday/Holiday/Program.cs
+++ Holiday/Holiday/Program.cs
@@ -4,19 +4,88 @@
     {
         static void Main(string[] args)
         {
-            int total;
-            Console.Write("Ededi daxil edin: ");
-            int number = int.Parse(Console.ReadLine()!);
+            /* 1. Verilmiş iki tam dəyərin cəmini hesablamaq üçün C# Sharp proqramını yazın.
+            İki dəyər eynidirsə, onların cəmini üç dəfə qaytarın*/
 
-            for(int i = 0; i <= 10; i++)
+            //Console.Write("1-ci ededi daxil edin: ");
+            //int number1 = int.Parse(Console.ReadLine()!);
+
+            //Console.Write("2-ci ededi daxil edin: ");
+            //int number2 = int.Parse(Console.ReadLine()!);
+
+            //if (number1 != number2)
+            //{
+            //    Console.Write("Netice: " + (number1 + number2));
+            //}
+            //else if (number1 == number2)
+            //{
+            //    Console.WriteLine(3 * (number1 + number2));
+            //}
+
+
+
+
+            /* 2. Verilmiş iki tam ədədi yoxlamaq üçün C# Sharp proqramını yazın 
+             və onlardan biri 30 və ya cəmi 30 olarsa, doğru qaytarın*/
+
+            //Console.Write("1-ci ededi daxil edin: ");
+            //int number1 = int.Parse(Console.ReadLine()!);
+
+            //Console.Write("2-ci ededi daxil edin: ");
+            //int number2 = int.Parse(Console.ReadLine()!);
+
+            //if (number1 == 30 || number2 == 30 || (number1 + number2) == 30)
+            //{
+            //    Console.WriteLine("Dogru");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Yanlis");
+            //}
+
+
+
+
+
+            /* 3. Uzunluğu 1 və ya daha çox olan verilmiş sətirin önünə və 
+            arxasına sonuncu simvol əlavə edilmiş yeni sətir yaratmaq üçün C# Sharp proqramını yazın.*/
+
+            //Console.Write("Metni daxil edin: ");
+            //string text = Console.ReadLine()!;
+
+            //for (int i = text.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(text[i] + text + text[i]);
+            //    break;
+            //}
+
+
+
+
+            /* 4. */
+
+            Console.Write("Metni daxil edin: ");
+            string text = Console.ReadLine()!;
+
+            for(int i = 0; i < text.Length; i++)
             {
-                if(i >= 0)
+                if (text[i] != 'y' && text[i+1] != 't')
                 {
-                    total = number * i;
-                    Console.WriteLine("Hasil: " + total);
+                    Console.Write(text[i]);
+                }
+                else if (text[0] == 'y' && text[1] == 't')
+                {
+                    Console.Write(text[i]);
+                }
+                else
+                {
+                    Console.Write(text);
                 }
             }
 
+
         }
+
     }
 }
